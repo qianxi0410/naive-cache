9a3ac2123960af3a433c2239e9c790c85bc4f34d
qianxi0410 <894871277@qq.com>
2021-12-08 19:34:36 +0800

    feat: add http handle

---
+++ http.go
@@ -0,0 +1,63 @@
+package naive_cache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_naive/"
+
+// HttpPool implements PeerPicker for a pool of HTTP peers.
+type HttpPool struct {
+	// self addr
+	self     string
+	basePath string
+}
+
+// NewHttpPool is a construct of HttpPool
+func NewHttpPool(self string) *HttpPool {
+	return &HttpPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+// Log info with server name
+func (p *HttpPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+// ServeHTTP impl the interface of http.handler
+func (p *HttpPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HttpPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	// /basepath/groupname/key
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}

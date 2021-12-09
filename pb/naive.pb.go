b901538fea6cdc3777ca68cbb15e549e6e63f4b6
qianxi0410 <894871277@qq.com>
2021-12-09 22:20:52 +0800

    chore: delete unused code

--- pb/naive.pb.go
+++ pb/naive.pb.go
@@ -7,10 +7,6 @@
 package pb
 
 import (
-	context "context"
-	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -135,10 +131,7 @@ var file_naive_proto_rawDesc = []byte{
 	0x75, 0x70, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
 	0x03, 0x6b, 0x65, 0x79, 0x22, 0x20, 0x0a, 0x08, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
 	0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52,
-	0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x32, 0x2e, 0x0a, 0x0a, 0x4e, 0x61, 0x69, 0x76, 0x65, 0x43,
-	0x61, 0x63, 0x68, 0x65, 0x12, 0x20, 0x0a, 0x03, 0x47, 0x65, 0x74, 0x12, 0x0b, 0x2e, 0x70, 0x62,
-	0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x0c, 0x2e, 0x70, 0x62, 0x2e, 0x52, 0x65,
-	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2e, 0x2f, 0x70, 0x62, 0x62,
+	0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x2e, 0x2f, 0x70, 0x62, 0x62,
 	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
@@ -160,10 +153,8 @@ var file_naive_proto_goTypes = []interface{}{
 	(*Response)(nil), // 1: pb.Response
 }
 var file_naive_proto_depIdxs = []int32{
-	0, // 0: pb.NaiveCache.Get:input_type -> pb.Request
-	1, // 1: pb.NaiveCache.Get:output_type -> pb.Response
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -208,7 +199,7 @@ func file_naive_proto_init() {
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
-			NumServices:   1,
+			NumServices:   0,
 		},
 		GoTypes:           file_naive_proto_goTypes,
 		DependencyIndexes: file_naive_proto_depIdxs,
@@ -218,84 +209,4 @@ func file_naive_proto_init() {
 	file_naive_proto_rawDesc = nil
 	file_naive_proto_goTypes = nil
 	file_naive_proto_depIdxs = nil
-}
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ context.Context
-var _ grpc.ClientConnInterface
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
-
-// NaiveCacheClient is the client API for NaiveCache service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type NaiveCacheClient interface {
-	Get(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
-}
-
-type naiveCacheClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewNaiveCacheClient(cc grpc.ClientConnInterface) NaiveCacheClient {
-	return &naiveCacheClient{cc}
-}
-
-func (c *naiveCacheClient) Get(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
-	err := c.cc.Invoke(ctx, "/pb.NaiveCache/Get", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// NaiveCacheServer is the server API for NaiveCache service.
-type NaiveCacheServer interface {
-	Get(context.Context, *Request) (*Response, error)
-}
-
-// UnimplementedNaiveCacheServer can be embedded to have forward compatible implementations.
-type UnimplementedNaiveCacheServer struct {
-}
-
-func (*UnimplementedNaiveCacheServer) Get(context.Context, *Request) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
-}
-
-func RegisterNaiveCacheServer(s *grpc.Server, srv NaiveCacheServer) {
-	s.RegisterService(&_NaiveCache_serviceDesc, srv)
-}
-
-func _NaiveCache_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Request)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(NaiveCacheServer).Get(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/pb.NaiveCache/Get",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(NaiveCacheServer).Get(ctx, req.(*Request))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _NaiveCache_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "pb.NaiveCache",
-	HandlerType: (*NaiveCacheServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "Get",
-			Handler:    _NaiveCache_Get_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "naive.proto",
 }
